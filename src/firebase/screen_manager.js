45fd11609b77469aa172f533b4ee0b18cbe70096
“Shiv <stavangandhi3008@gmail.com>
2025-03-06 17:06:34 -0600

    Added Home ScreenManager Func

--- src/firebase/screen_manager.js
+++ src/firebase/screen_manager.js
@@ -1,3 +1,4 @@
+import { doc } from "firebase/firestore";
 import { auth } from "./config.js";
 import { onAuthStateChanged } from "firebase/auth";
 
@@ -19,6 +20,7 @@ onAuthStateChanged(auth, (user) => {
       window.location.replace(`${winLocation.origin}${pathname.auth}#${winHash}`);
     } else if (winLocation.pathname != pathname.home) {
       window.location.replace(`${winLocation.origin}${pathname.home}#${winHash}`);
+      onHashChange();
     }
   } else {
     if (winLocation.pathname != pathname.auth) {
@@ -27,6 +29,23 @@ onAuthStateChanged(auth, (user) => {
   }
 
   console.log(winLocation);
+});
+
+function onHashChange() {
+  document.getElementById("HomeView").classList.add("d-none");
+  document.getElementById("ExploreView").classList.add("d-none");
+  document.getElementById("ActivityView").classList.add("d-none");
+  document.getElementById("ChatView").classList.add("d-none");
+
+  if (window.location.hash.slice(1) == ""){
+    document.getElementById("HomeView").classList.remove("d-none");
+  } else {
+    document.getElementById(`${window.location.hash.slice(1)}View`).classList.remove("d-none");
+  }
+}
+
+window.addEventListener('hashchange', function() {
+  onHashChange();
 });
 
 export function getCurrentScreen() {
